531dc8065b697bb4e14f4b67446e9c05a7540de2
= <nurlanam@code.edu.az>
2021-06-11 09:31:20 +0400

    add initial structure

---
+++ UniSubscription/src/main/java/az/code/unisubscription/UniSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package az.code.unisubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniSubscriptionApplication.class, args);
+    }
+
+}
---
+++ UniSubscription/src/test/java/az/code/unisubscription/UniSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package az.code.unisubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
