cb8219150ae351e0316a2e0c8323f199483c5959
Amrah-Ahmadov <83633301+Amrah-Ahmadov@users.noreply.github.com>
2021-06-12 14:21:44 +0400

    send mail 1 day before subscription expires

--- UniSubscription/src/main/java/az/code/unisubscription/repositories/SubscriptionRepository.java
+++ UniSubscription/src/main/java/az/code/unisubscription/repositories/SubscriptionRepository.java
@@ -4,6 +4,7 @@ import az.code.unisubscription.models.Subscription;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SubscriptionRepository extends CrudRepository<Subscription, Integer> {
